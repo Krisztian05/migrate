eacb1b17bf6fbaa70e525d599ce5514e82047fe2
Krisztian05 <113445750+Krisztian05@users.noreply.github.com>
2022-09-26 09:15:43 +0200

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,10 +1,10 @@
-net use Z: \\pdc\Diakkaptar\
-Z:\
+net use Z: \\PDC\Diakkaptar
+Z:
 cd "Z:\Steyer Zalán\11linux\12ABsz3"
 copy .\12AB_sz3.ova C:\Users\horvath.krisztian2\Documents\12AB.ova
 notepad .\12ABSZ3_settings.txt
 notepad .\parancsok.txt
-C:\
+C:
 cd C:\Program Files\Oracle\VirtualBox
 vboxmanage import C:\Users\horvath.krisztian2\Documents\12AB.ova
 exit
