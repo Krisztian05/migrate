6c85245b358e13495ace69dc99266f4dbc293908
Krisztian05 <113445750+Krisztian05@users.noreply.github.com>
2022-09-28 20:38:04 +0200

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,11 +1,11 @@
 net use Z: \\PDC\Diakkaptar
 Z:
 cd "Z:\Steyer Zalán\11linux\12ABsz3"
-copy .\12AB_sz3.ova C:\Users\horvath.krisztian2\Documents\12AB.ova
+copy .\12AB_sz3.ova C:\Users\%username%\Documents\12AB.ova
 notepad .\12ABSZ3_settings.txt
 notepad .\parancsok.txt
 C:
 cd "C:\Program Files\Oracle\VirtualBox"
-.\VBoxManage.exe import C:\Users\horvath.krisztian2\Documents\12AB.ova
+.\VBoxManage.exe import C:\Users\%username%\Documents\12AB.ova
 start VirtualBox.exe
 exit
